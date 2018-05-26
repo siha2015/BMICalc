2eddba5df77cc2cb9a2566f3650d4cca93230b01
siha2015 <sihag1412@gmail.com>
2018-05-26 17:08:59 +0530

    first commit

---
+++ app/src/main/java/com/healthzone/bmicalc/Overweight.java
@@ -0,0 +1,13 @@
+package com.healthzone.bmicalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Overweight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overweight);
+    }
+}
---
+++ app/src/main/java/com/healthzone/bmicalc/Underweight.java
@@ -0,0 +1,20 @@
+package com.healthzone.bmicalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class Underweight extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_underweight);
+
+
+    }
+
+}
+
